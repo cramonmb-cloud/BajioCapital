aa04c7280ef1d544b84480213db2db38abffc834
C. Ramón Moran <marketingadsp@gmail.com>
2025-12-18 22:54:18 +0000

    Error: ./src/ai/dev.ts:5:1 [1m[31mModule not found[39m[22m: Can't re

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,7 +2,7 @@
  * @fileoverview This file is loaded only in development and is used to load development-specific plugins.
  */
 import {genkit} from 'genkit';
-import {dotprompt, typescript} from 'genkitx-dotprompt';
+import {dotprompt, typescript} from '@genkit-ai/dotprompt';
 
 export const dev = genkit({
   plugins: [
