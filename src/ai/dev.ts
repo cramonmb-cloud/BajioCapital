bc52b36bc570959952dfcb5b225e108be3723de3
C. Ramón Moran <marketingadsp@gmail.com>
2025-12-18 22:59:20 +0000

    el error sigue y sigue y no lo solucionas

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,11 +2,11 @@
  * @fileoverview This file is loaded only in development and is used to load development-specific plugins.
  */
 import {genkit} from 'genkit';
-import {dotprompt, typescript} from '@genkit-ai/dotprompt';
+import {Dotprompt, typescript} from '@genkit-ai/dotprompt';
 
 export const dev = genkit({
   plugins: [
-    dotprompt({
+    Dotprompt({
       model: 'googleai/gemini-1.5-flash',
       prompt: {
         model: 'googleai/gemini-1.5-flash',
