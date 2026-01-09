c2903a7298f5f8b515bccf18c56d9b51c33897a1
C. Ramón Moran <marketingadsp@gmail.com>
2026-01-09 05:48:28 +0000

    Ayudame a que mi app se pueda instalar como pwa, haciendo que el icono s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: appName,
     description: description,
+    manifest: '/manifest.ts',
     openGraph: {
       title: appName,
       description: description,
@@ -40,6 +41,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
